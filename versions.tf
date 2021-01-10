d581e33ffe23bd821e94954778011ba46123e63d
Mickaël Canévet <mickael.canevet@gmail.com>
2021-01-10 10:25:31 +0100

    Fix version constraints

--- versions.tf
+++ versions.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.0.0"
+      version = "~> 2.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0.0"
+      version = "~> 3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0.0"
+      version = "~> 3.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.8.0"
+      version = "~> 2.8"
     }
   }
   required_version = ">= 0.13"
